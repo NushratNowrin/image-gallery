4cb8ceddeaedba386b2db38be7db30f076c0c569
NushratNowrin <nowrin2023@gmail.com>
2023-11-02 00:53:22 +0600

    image positioning done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import img1 from "../src/assets/images/image-1.webp";
-import img2 from "../src/assets/images/image-2.webp";
-import img3 from "../src/assets/images/image-3.webp";
-import img4 from "../src/assets/images/image-4.webp";
+import img1 from "../src/assets/images/image-11.jpeg";
+import img2 from "../src/assets/images/image-3.webp";
+import img3 from "../src/assets/images/image-2.webp";
+import img4 from "../src/assets/images/image-7.webp";
 import img5 from "../src/assets/images/image-5.webp";
-import img6 from "../src/assets/images/image-6.webp";
-import img7 from "../src/assets/images/image-7.webp";
-import img8 from "../src/assets/images/image-8.webp";
-import img9 from "../src/assets/images/image-9.webp";
-import img10 from "../src/assets/images/image-10.jpeg";
-import img11 from "../src/assets/images/image-11.jpeg";
+import img6 from "../src/assets/images/image-9.webp";
+import img7 from "../src/assets/images/image-10.jpeg";
+import img8 from "../src/assets/images/image-4.webp";
+import img9 from "../src/assets/images/image-8.webp";
+import img10 from "../src/assets/images/image-1.webp";
+
 import "./App.css";
 function App() {
 	return (
@@ -16,7 +16,7 @@ function App() {
 			<div className='bg-white p-8 border border-gray-300 shadow-md rounded-xl'>
 				<div className='grid grid-cols-5 gap-5'>
 					<div className='col-span-2 row-span-2 gallery-img'>
-						<img src={img11} alt='' />
+						<img src={img1} alt='' />
 					</div>
 					<div className="gallery-img">
 						<img src={img2} alt='' />
@@ -46,7 +46,7 @@ function App() {
 						<img src={img10} alt='' />
 					</div>
 					<div className="gallery-img">
-						<img src={img1} alt='' />
+						<img src={img2} alt='' />
 					</div>
 				</div>
 			</div>
