ec19372a8fdd42b3816ddde75a6dc5db59e71694
NushratNowrin <nowrin2023@gmail.com>
2023-11-02 00:43:01 +0600

    simple gallery is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,54 @@
+import img1 from "../src/assets/images/image-1.webp";
+import img2 from "../src/assets/images/image-2.webp";
+import img3 from "../src/assets/images/image-3.webp";
+import img4 from "../src/assets/images/image-4.webp";
+import img5 from "../src/assets/images/image-5.webp";
+import img6 from "../src/assets/images/image-6.webp";
+import img7 from "../src/assets/images/image-7.webp";
+import img8 from "../src/assets/images/image-8.webp";
+import img9 from "../src/assets/images/image-9.webp";
+import img10 from "../src/assets/images/image-10.jpeg";
+import img11 from "../src/assets/images/image-11.jpeg";
 import "./App.css";
 function App() {
 	return (
-		<div className='bg-slate-200 h-screen p-10'>
-			<div className='bg-white h-screen border border-gray-300 shadow-md rounded-xl'>
-				<div>
-          
-        </div>
+		<div className='bg-slate-200 p-10'>
+			<div className='bg-white p-8 border border-gray-300 shadow-md rounded-xl'>
+				<div className='grid grid-cols-5 gap-5'>
+					<div className='col-span-2 row-span-2 gallery-img'>
+						<img src={img11} alt='' />
+					</div>
+					<div className="gallery-img">
+						<img src={img2} alt='' />
+					</div>
+					<div className="gallery-img">
+						<img src={img3} alt='' />
+					</div>
+					<div className="gallery-img">
+						<img src={img4} alt='' />
+					</div>
+					<div className="gallery-img">
+						<img src={img5} alt='' />
+					</div>
+					<div className="gallery-img">
+						<img src={img6} alt='' />
+					</div>
+					<div className="gallery-img">
+						<img src={img7} alt='' />
+					</div>
+					<div className="gallery-img">
+						<img src={img8} alt='' />
+					</div>
+					<div className="gallery-img">
+						<img src={img9} alt='' />
+					</div>
+					<div className="gallery-img">
+						<img src={img10} alt='' />
+					</div>
+					<div className="gallery-img">
+						<img src={img1} alt='' />
+					</div>
+				</div>
 			</div>
 		</div>
 	);
