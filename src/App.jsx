591ad9da915a15e6e947f53e599e4d6db138aa2c
NushratNowrin <nowrin2023@gmail.com>
2023-11-02 00:26:57 +0600

    bg ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-
-import './App.css'
-
+import "./App.css";
 function App() {
-
-
-  return (
-    <>
-     
-    </>
-  )
+	return (
+		<div className='bg-slate-200 h-screen p-10'>
+			<div className='bg-white h-screen border border-gray-300 shadow-md rounded-xl'>
+				<div>
+          
+        </div>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
